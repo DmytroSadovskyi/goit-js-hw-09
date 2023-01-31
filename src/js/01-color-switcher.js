c48f1c078b7d3c7c391130868b3d4bee339970a6
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-01-31 13:22:49 +0200

    task-1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+//  пошук елементів у DOM
+const startBtn = document.querySelector('button[data-start]');
+const stoptBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
+// Запис початкового значення intervalId
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+// створення колбек-функцій для слухачів подій для кнопок Start і Stop
+const onStartBtnClick = () => {
+  intervalId = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    body.style.backgroundColor = randomColor;
+  }, 1000);
+  startBtn.setAttribute('disabled', true);
+  stoptBtn.removeAttribute('disabled', true);
+};
+
+const onStopBtnClick = () => {
+  clearInterval(intervalId);
+  startBtn.removeAttribute('disabled', true);
+  stoptBtn.setAttribute('disabled', true);
+};
+
+// додавання слухачів подій для кнопок
+startBtn.addEventListener('click', onStartBtnClick);
+stoptBtn.addEventListener('click', onStopBtnClick);
