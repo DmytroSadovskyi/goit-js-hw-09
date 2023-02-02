a4670d1cdf42a454b63ca1990f5b571d5ca03a41
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-02-02 12:38:02 +0200

    task-2 done

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,12 +1,18 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const dateInput = document.getElementById('datetime-picker');
 const startCountdownBtn = document.querySelector('[data-start]');
-const daysSpan = document.getElementById('data-days');
-const hoursSpan = document.getElementById('data-hours');
-const minutesSpan = document.getElementById('data-minutes');
-const secondsSpan = document.getElementById('data-seconds');
+const daysSpan = document.querySelector('[data-days]');
+const hoursSpan = document.querySelector('[data-hours]');
+const minutesSpan = document.querySelector('[data-minutes]');
+const secondsSpan = document.querySelector('[data-seconds]');
+
+let targetTime = null;
+let startTime = null;
+let timerId = null;
+startCountdownBtn.disabled = true;
 
 const options = {
   enableTime: true,
@@ -14,76 +20,62 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    onCheckDate(selectedDates);
   },
 };
 
-flatpickr(dateInput, options);
-
-class Timer {
-  constructor({ onTick }) {
-    this.intervalId = null;
-    this.isActive = false;
-    this.onTick = onTick;
-
-    this.init();
-  }
-
-  init() {
-    const time = this.convertMs(0);
-    this.onTick(time);
-  }
-
-  start() {
-    if (this.isActive) {
-      return;
-    }
-
-    const startTime = Date.now();
-    this.isActive = true;
-
-    this.intervalId = setInterval(() => {
-      const currentTime = selectedDates[0];
-      const deltaTime = currentTime - startTime;
-      const time = this.convertMs(deltaTime);
-
-      this.onTick(time);
-    }, 1000);
-  }
-
-  convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-
-    // Remaining days
-    const days = this.addLeadingZero(Math.floor(ms / day));
-    // Remaining hours
-    const hours = this.addLeadingZero(Math.floor((ms % day) / hour));
-    // Remaining minutes
-    const minutes = this.addLeadingZero(
-      Math.floor(((ms % day) % hour) / minute)
-    );
-    // Remaining seconds
-    const seconds = this.addLeadingZero(
-      Math.floor((((ms % day) % hour) % minute) / second)
-    );
-
-    return { days, hours, minutes, seconds };
-  }
-
-  addLeadingZero(value) {
-    return String(value).padStart(2, '0');
+function onCheckDate(selectedDates) {
+  targetTime = selectedDates[0].getTime();
+  startTime = new Date().getTime();
+  if (targetTime < startTime) {
+    Notify.failure('Please choose a date in the future');
+  } else {
+    Notify.success('Now you can launch your timer!');
+    startCountdownBtn.disabled = false;
   }
 }
+flatpickr(dateInput, options);
 
-const timer = new Timer({
-  onTick: updateClockface,
-});
+const onStartBtnClick = () => {
+  timerId = setInterval(() => {
+    startTime = new Date().getTime();
+    const deltaTime = targetTime - startTime;
 
-startCountdownBtn.addEventListener('click', timer.start.bind(timer));
+    if (deltaTime <= 0) {
+      clearInterval(timerId);
+    } else {
+      const time = convertMs(deltaTime);
+      updateClockface(time);
+    }
+  }, 1000);
+};
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+startCountdownBtn.addEventListener('click', onStartBtnClick);
 
 function updateClockface({ days, hours, minutes, seconds }) {
   daysSpan.textContent = days;
