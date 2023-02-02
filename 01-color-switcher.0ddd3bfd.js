1b1794b352d6feef52a419191f85392e4e62f86d
DmytroSadovskyi <111589023+DmytroSadovskyi@users.noreply.github.com>
2023-02-02 10:38:29 +0000

    Deploying to gh-pages from @ DmytroSadovskyi/goit-js-hw-09@a4670d1cdf42a454b63ca1990f5b571d5ca03a41 🚀

--- 01-color-switcher.0ddd3bfd.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body"),n=null;t.addEventListener("click",(function(){n=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));r.style.backgroundColor=t}),1e3),t.setAttribute("disabled",!1),e.removeAttribute("disabled",!0)})),e.addEventListener("click",(function(){clearInterval(n),t.removeAttribute("disabled",!0),e.setAttribute("disabled",!1)}))}();
-//# sourceMappingURL=01-color-switcher.0ddd3bfd.js.map
